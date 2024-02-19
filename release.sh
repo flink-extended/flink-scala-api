daea10c88b7045cfa2c00a2dd580af33368f7bb9
Alexey Novakov <mein-eingang@eclipso.eu>
2024-02-19 16:22:29 +0100

    upgrade patch release for 1.18

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 set -ex
 RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.16.3 test 'release with-defaults'
 RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.17.2 test 'release with-defaults'
-RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.18.0 test 'release with-defaults'
+RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.18.1 test 'release with-defaults'
 wait
