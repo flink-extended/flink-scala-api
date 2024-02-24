6b211929637cb5bfe39978d8b201534e88f01c64
buinauskas <buinauskas.evaldas@gmail.com>
2024-02-24 05:57:56 +0200

    Switch to using integration test mixin

--- src/test/scala/org/apache/flinkx/api/ExampleTest.scala
+++ src/test/scala/org/apache/flinkx/api/ExampleTest.scala
@@ -1,50 +1,17 @@
 package org.apache.flinkx.api
 
-import org.apache.flinkx.api.serializers._
-import org.apache.flink.api.common.RuntimeExecutionMode
-import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration
-import org.apache.flink.streaming.api.datastream.DataStreamSource
-import org.apache.flink.test.util.MiniClusterWithClientResource
-import org.scalatest.BeforeAndAfterAll
+import org.apache.flinkx.api.serializers._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import scala.annotation.nowarn
-
-class ExampleTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
+class ExampleTest extends AnyFlatSpec with Matchers with IntegrationTest {
   import ExampleTest._
-
-  lazy val cluster = new MiniClusterWithClientResource(
-    new MiniClusterResourceConfiguration.Builder().setNumberSlotsPerTaskManager(1).setNumberTaskManagers(1).build()
-  )
-
-  lazy val env: StreamExecutionEnvironment = {
-    cluster.getTestEnvironment.setAsContext()
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setParallelism(1)
-    env.setRuntimeMode(RuntimeExecutionMode.STREAMING)
-    env.enableCheckpointing(1000)
-    env.setRestartStrategy(RestartStrategies.noRestart())
-    env.getConfig.disableGenericTypes()
-    env
-  }
-
-  override protected def beforeAll(): Unit = {
-    super.beforeAll()
-    cluster.before()
-  }
-
-  override def afterAll(): Unit = {
-    cluster.after()
-    super.afterAll()
-  }
 
   it should "run example code" in {
     val result = env
-      .fromScalaCollection(List(Event.Click("1"), Event.Purchase(1.0)))
-      .executeAndCollect(10)
+      .fromCollection(List(Event.Click("1"), Event.Purchase(1.0)))
+      .collect()
 
     result.size shouldBe 2
   }
@@ -58,13 +25,5 @@ object ExampleTest {
     final case class Purchase(price: Double) extends Event
 
     implicit val eventTypeInfo: TypeInformation[Event] = deriveTypeInformation[Event]
-  }
-
-  implicit final class EnvOps(private val env: StreamExecutionEnvironment) extends AnyVal {
-    import scala.collection.JavaConverters._
-
-    @nowarn("cat=deprecation")
-    def fromScalaCollection[A](data: Seq[A])(implicit typeInformation: TypeInformation[A]): DataStreamSource[A] =
-      env.getJavaEnv.fromCollection(data.asJava, typeInformation)
   }
 }
