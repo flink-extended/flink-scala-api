50358dbdf6222a30139bd5fd90df7a87a3a5b97f
novakov-alexey <117519424+novakov-alexey@users.noreply.github.com>
2024-05-06 10:26:38 +0200

    Call duplicate when type is not immutable (#114)
    call duplicate when serializer is not immutable for some of the type infos
    
    ---------
    
    Co-authored-by: Suno <suno@hpcnt.com>

--- src/main/scala/org/apache/flinkx/api/typeinfo/CoproductTypeInformation.scala
+++ src/main/scala/org/apache/flinkx/api/typeinfo/CoproductTypeInformation.scala
@@ -5,11 +5,13 @@ import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.common.typeutils.TypeSerializer
 
 case class CoproductTypeInformation[T](c: Class[T], ser: TypeSerializer[T]) extends TypeInformation[T] {
-  override def createSerializer(config: ExecutionConfig): TypeSerializer[T] = ser
-  override def isBasicType: Boolean                                         = false
-  override def isTupleType: Boolean                                         = false
-  override def isKeyType: Boolean                                           = false
-  override def getTotalFields: Int                                          = 1
-  override def getTypeClass: Class[T]                                       = c
-  override def getArity: Int                                                = 1
+  override def createSerializer(config: ExecutionConfig): TypeSerializer[T] =
+    if (ser.isImmutableType) ser
+    else ser.duplicate()
+  override def isBasicType: Boolean   = false
+  override def isTupleType: Boolean   = false
+  override def isKeyType: Boolean     = false
+  override def getTotalFields: Int    = 1
+  override def getTypeClass: Class[T] = c
+  override def getArity: Int          = 1
 }
