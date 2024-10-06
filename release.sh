dd90af27de6fed0f17d9cd194ad243a8fae2fabd
Alexey Novakov <mein-eingang@eclipso.eu>
2024-10-06 21:35:29 +0200

    update release process documentation as per new sbt project structure

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.18.1 test 'release with-defaults'
-RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.19.1 test 'release with-defaults'
-RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.20.0 test 'release with-defaults'
+RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.18.1 -Dgpg.passphrase="<phrase>" test "; project scala-api; release with-defaults"
+RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.19.1 -Dgpg.passphrase="<phrase>" test "; project scala-api; release with-defaults"
+RELEASE_VERSION_BUMP=true sbt -DflinkVersion=1.20.0 -Dgpg.passphrase="<phrase>" test "; project scala-api; release with-defaults"
 wait
