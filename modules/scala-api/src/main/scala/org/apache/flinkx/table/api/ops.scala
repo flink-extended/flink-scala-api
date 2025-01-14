32b93c709a9c9d3c93da5701f71b14c1a03ce48c
Alexey Novakov <mein-eingang@eclipso.eu>
2025-01-14 23:57:30 +0100

    wrap in object

--- modules/scala-api/src/main/scala/org/apache/flinkx/table/api/ops.scala
+++ modules/scala-api/src/main/scala/org/apache/flinkx/table/api/ops.scala
@@ -2,11 +2,16 @@ package org.apache.flinkx.table.api
 
 import org.apache.flinkx.api.DataStream
 
+import org.apache.flink.annotation.Experimental
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment
 import org.apache.flink.table.api.Table
 import org.apache.flink.types.Row
+import org.apache.flink.streaming.api.datastream.{DataStream => JavaDataStream}
 
-implicit class ops(tEnv: StreamTableEnvironment) {
-  def toStream(table: Table): DataStream[Row] =
-    DataStream(tEnv.toDataStream(table))
+@Experimental
+object ops {
+  implicit class StreamTableEnvironmentOps(tEnv: StreamTableEnvironment) {
+    def toStream(table: Table): DataStream[Row] =
+      new DataStream(tEnv.toDataStream(table))
+  }
 }
